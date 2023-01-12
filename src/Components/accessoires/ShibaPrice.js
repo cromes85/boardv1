1e16deb26f1e31fcb7f0147e3860a06f48dd0560
cromes85 <idv230685@gmail.com>
2023-01-12 20:44:47 +0100

    add v6

--- src/Components/accessoires/ShibaPrice.js
+++ src/Components/accessoires/ShibaPrice.js
@@ -3,6 +3,7 @@ import React, { useState, useEffect } from "react";
 function ShibaPrice() {
   const [price, setPrice] = useState(null);
   const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const [shibaAmount, setShibaAmount] = useState(0);
   const [applyCommission, setApplyCommission] = useState(false);
   const [commissionRate, setCommissionRate] = useState(1.49);
@@ -10,10 +11,9 @@ function ShibaPrice() {
 
   useEffect(() => {
     async function fetchData() {
+      setIsLoading(true);
       try {
-        const response = await fetch(
-          "https://api.coingecko.com/api/v3/coins/shiba-inu"
-        );
+        const response = await fetch("https://api.coingecko.com/api/v3/coins/shiba-inu");
         if (!response.ok) {
           throw new Error(response.statusText);
         }
@@ -26,9 +26,11 @@ function ShibaPrice() {
       } catch (e) {
         setError(e.message);
       } finally {
+        setIsLoading(false);
         timeoutId = setTimeout(fetchData, 5000);
       }
     }
+    
     fetchData();
     return () => clearTimeout(timeoutId);
   }, []);
@@ -46,45 +48,47 @@ function ShibaPrice() {
   }
 
   return (
-    <div>
-      <h2>Prix actuel du Shiba Inu :</h2>
-      {price ? <p>{price} €</p> : error ? <p>{error}</p> : <p>Chargement...</p>}
-
-      <label htmlFor="shiba-amount">Nombre de pièces de Shiba Inu :</label>
-      <input
-        type="number"
-        id="shiba-amount"
-        value={shibaAmount}
-        onChange={handleAmountChange}
-      />
-      {price && <p>Total en euros : {shibaAmount * price} €</p>}
-
-      <label>
-        Appliquer une commission
+    
+      <div>
+        <h2>Prix actuel du Shiba Inu :</h2>
+        {isLoading ? <p>Chargement...</p> : price ? <p>{price} €</p> : error ? <p>{error}</p> : null}
+  
+        <label htmlFor="shiba-amount">Nombre de pièces de Shiba Inu :</label>
         <input
-          type="checkbox"
-          checked={applyCommission}
-          onChange={handleCommissionChange}
+          type="number"
+          id="shiba-amount"
+          value={shibaAmount}
+          onChange={handleAmountChange}
         />
-      </label>
-
-      {applyCommission && (
-        <div>
-          <label htmlFor="commission-rate">Taux de commission :</label>
+        {price && <p>Total en euros : {shibaAmount * price} €</p>}
+  
+        <label>
+          Appliquer une commission
           <input
-            type="number"
-            id="commission-rate"
-            value={commissionRate}
-            onChange={handleCommissionRateChange}
+            type="checkbox"
+            checked={applyCommission}
+            onChange={handleCommissionChange}
           />
-          <p>
-            Prix de vente :{" "}
-            {shibaAmount * price - shibaAmount * price * (commissionRate / 100)}{" "}
-            €
-          </p>
-        </div>
-      )}
-    </div>
-  );
+        </label>
+  
+        {applyCommission && (
+          <div>
+            <label htmlFor="commission-rate">Taux de commission :</label>
+            <input
+              type="number"
+              id="commission-rate"
+              value={commissionRate}
+              onChange={handleCommissionRateChange}
+            />
+            <p>
+              Prix de vente :{" "}
+              {shibaAmount * price - shibaAmount * price * (commissionRate / 100)}{" "}
+              €
+            </p>
+          </div>
+        )}
+      </div>
+    );
+  
 }
 export default ShibaPrice;
