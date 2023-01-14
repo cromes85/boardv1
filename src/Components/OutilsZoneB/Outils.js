d3321c3b2762bce7227d4364031fc3ad19151705
cromes85 <idv230685@gmail.com>
2023-01-14 14:15:01 +0100

    test14012023

--- src/Components/OutilsZoneB/Outils.js
+++ src/Components/OutilsZoneB/Outils.js
@@ -6,7 +6,7 @@ const Outils = () => {
             <ul class="grid grid-low gap-3">
                 <li>
                     <a href="https://chat.openai.com/chat"></a>
-                    <img class="w-10 h-10 rounded-full" src="../../img/ia.jpg" alt=""></img>
+                    <img class="w-10 h-10 rounded-full" src="../../img/icons/ia.jpg" alt="Rounded avatar"/>
                 </li>
                 <li>
                     <a href="https://www.google.com">Google</a>
