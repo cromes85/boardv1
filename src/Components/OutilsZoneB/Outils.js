d316309da73665d1bd69038931d40ccdbba03281
cromes85 <idv230685@gmail.com>
2023-01-14 14:19:54 +0100

    rr

--- src/Components/OutilsZoneB/Outils.js
+++ src/Components/OutilsZoneB/Outils.js
@@ -1,20 +1,23 @@
-import React from 'react';
+import React from "react";
 
 const Outils = () => {
-    return (
-        <div>
-            <ul class="grid grid-low gap-3">
-                <li>
-                    <a href="https://chat.openai.com/chat"></a>
-                    <img class="w-10 h-10 rounded-full" src="../../img/icons/ia.jpg" alt="Rounded avatar"/>
-                </li>
-                <li>
-                    <a href="https://www.google.com">Google</a>
-                </li>
-
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <ul class="grid grid-low gap-3">
+        <li>
+          <a href="https://chat.openai.com/chat"></a>
+          <img
+            class="w-10 h-10 rounded-full"
+            src="./ia.jpg"
+            alt="Rounded avatar"
+          />
+        </li>
+        <li>
+          <a href="https://www.google.com">Google</a>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Outils;
