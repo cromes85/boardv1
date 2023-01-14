6637d8fd677081e4331cebe2aa29f18cb9a2acb0
cromes85 <idv230685@gmail.com>
2023-01-14 14:26:51 +0100

    tut

--- src/Components/OutilsZoneB/Outils.js
+++ src/Components/OutilsZoneB/Outils.js
@@ -5,15 +5,25 @@ const Outils = () => {
     <div>
       <ul class="grid grid-low gap-3">
         <li>
-          <a href="https://chat.openai.com/chat"> <img
-            class="w-10 h-10 rounded-full"
-            src="./ia.jpg"
-            alt="Rounded avatar"
-          /></a>
-          
+          <a href="https://chat.openai.com/chat">
+            {" "}
+            <img
+              class="w-10 h-10 rounded-full"
+              src="./ia.jpg"
+              alt="Rounded avatar"
+              target="_blank"
+              rel="noopener noreferrer"
+            />
+          </a>
         </li>
         <li>
-          <a href="https://www.google.com">Google</a>
+          <a
+            href="https://www.google.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Google
+          </a>
         </li>
       </ul>
     </div>
