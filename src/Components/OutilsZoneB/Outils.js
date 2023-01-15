e95baf3fbfb16a47cbd53f14a1e36092149f10d9
cromes85 <idv230685@gmail.com>
2023-01-15 11:32:21 +0100

    g

--- src/Components/OutilsZoneB/Outils.js
+++ src/Components/OutilsZoneB/Outils.js
@@ -36,7 +36,7 @@ const Outils = () => {
           
             <NavLink to="/Greetings">
             <img
-            class="w-12 h-12 rounded-full object-scale-down "
+            class="w-10 h-10 rounded-full object-scale-down "
             src="./mem.png"
             
           />
