aedc35643decde64a127548d4c7301379cf67715
cromes85 <idv230685@gmail.com>
2023-01-15 01:35:48 +0100

    ljh

--- src/Components/accessoires/JokeScroller.js
+++ src/Components/accessoires/JokeScroller.js
@@ -6,20 +6,20 @@ function JokeScroller() {
 
   useEffect(() => {
     fetch("./blague.json")
-      .then(res => res.json())
-      .then(data => setJokes(data))
-      .catch(err => console.log(err));
+      .then((res) => res.json())
+      .then((data) => setJokes(data))
+      .catch((err) => console.log(err));
   }, []);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCurrentJoke(currentJoke => (currentJoke + 1) % jokes.length);
-    }, 86400000); // 86400000ms = 24h
+      setCurrentJoke((currentJoke) => (currentJoke + 1) % jokes.length);
+    }, 10000); // 86400000ms = 24h
     return () => clearInterval(interval);
   }, [jokes, currentJoke]);
 
   return (
-    <div  class="inline-block align-middle text-center">
+    <div class="inline-block align-middle text-center">
       <p class="text-xl">{jokes[currentJoke]?.joke}</p>
       <p class="text-xl">{jokes[currentJoke]?.answer}</p>
     </div>
