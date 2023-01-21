d9ff61bc859d328f0637eecbdb9ea989297b13c1
cromes85 <idv230685@gmail.com>
2023-01-21 13:22:05 +0100

    test

--- src/Components/OutilsZoneB/Office365.js
+++ src/Components/OutilsZoneB/Office365.js
@@ -40,7 +40,12 @@ function Office365(props) {
           Exchange
         </span>
       </a>
-      <a target="_blank" href="https://admin.microsoft.com/Adminportal/Home?source=applauncher#/users" rel="noopener noreferrer" onClick={handleClick}>
+      <a
+        target="_blank"
+        href="https://admin.microsoft.com/Adminportal/Home?source=applauncher#/users"
+        rel="noopener noreferrer"
+        onClick={handleClick}
+      >
         <span class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
           Users
         </span>
