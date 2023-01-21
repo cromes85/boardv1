4a391d78326d22885898566603d3cb3b30012a66
cromes85 <idv230685@gmail.com>
2023-01-21 12:35:03 +0100

    update

--- src/Components/accessoires/ClownHead.js
+++ src/Components/accessoires/ClownHead.js
@@ -1,30 +1,19 @@
 import React, { useState, useEffect } from 'react';
 
-import './clownHead.css';
-
-
 const ClownHead = () => {
-  const [isHovered, setIsHovered] = useState(false);
   const [animation, setAnimation] = useState('');
 
   useEffect(() => {
-    if (isHovered) {
-      setAnimation('swing 5s');
-    } else {
-      setAnimation('');
-    }
-  }, [isHovered]);
+    setAnimation('swing 5s');
+  }, []);
 
   return (
-    
     <div>
       <img
         style={{ animation: animation }}
         className="w-32 rounded-full"
         src="./crane.jpg"
         alt="Clown Head"
-        onMouseEnter={() => setIsHovered(true)}
-        onMouseLeave={() => setIsHovered(false)}
       />
     </div>
   );
