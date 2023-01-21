c1b5b00fe6fc2b082195bd0050e8b0f84a3d3a9c
cromes85 <idv230685@gmail.com>
2023-01-21 13:17:21 +0100

    on sait jamais

--- src/Components/OutilsZoneB/Office365.js
+++ src/Components/OutilsZoneB/Office365.js
@@ -11,70 +11,45 @@ function Office365(props) {
   return (
     <div class="grid grid-cols-1 lg:grid-cols-3 gap-4 content-center justify-items-center">
       <a
-              
-              href="https://admin.microsoft.com/Adminportal/Home?source=applauncher#/licenses"
-              
-              rel="noopener noreferrer"
-              onClick={handleClick}
-            >
-              <span class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Licences</span>
-              
-            </a>
-            <a
-              href="https://admin.exchange.microsoft.com/#/messagetrace"
-              
-              rel="noopener noreferrer"
-              onClick={handleClick}
-            >
-              <span class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Tracking</span>
-              
-            </a>
-
-            <a
-              href="https://admin.exchange.microsoft.com/#/mailboxes"
-              
-              rel="noopener noreferrer"
-              onClick={handleClick}
-            >
-              <span class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Exchange</span>
-            </a>
-
-            <a
-              href="https://admin.microsoft.com/Adminportal/Home?source=applauncher#/users"
-              target="_blank"
-              rel="noopener noreferrer"
-              onClick={handleClick}
-            >
-              <span class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Users</span>
-            </a>
-
-            <a
-              href=""
-              target="_blank"
-              rel="noopener noreferrer"
-              onClick={handleClick}
-            >
-              <span class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Quarantaine</span>
-            </a>
-
-            <a
-              href="https://admin.microsoft.com/Adminportal/Home?invType=IndirectResellerRelationship&partnerId=70831ff1-16f0-4aeb-a0e3-456aeeb5c0ec&msppId=601089&DAP=true#/BillingAccounts/partner-invitation"
-              target="_blank"
-              rel="noopener noreferrer"
-              onClick={handleClick}
-            >
-              <span class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Ingram1</span>
-            </a>
-
-            <a
-              href="https://admin.microsoft.com/Adminportal/Home?invType=IndirectResellerRelationship&partnerId=70831ff1-16f0-4aeb-a0e3-456aeeb5c0ec&msppId=601089&DAP=true&indirectCSPId=abd71061-286d-497f-bb59-9f07d2e5c8a7#/BillingAccounts/partner-invitationhttps://admin.microsoft.com/Adminportal/Home?invType=IndirectResellerRelationship&partnerId=70831ff1-16f0-4aeb-a0e3-456aeeb5c0ec&msppId=601089&DAP=true&indirectCSPId=abd71061-286d-497f-bb59-9f07d2e5c8a7#/BillingAccounts/partner-invitation"
-              target="_blank"
-              rel="noopener noreferrer"
-              onClick={handleClick}
-            >
-              <span class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Ingram2</span>
-            </a>
-      
+        class="block lg:grid col-span-12"
+        href="https://admin.microsoft.com/Adminportal/Home?source=applauncher#/licenses"
+        rel="noopener noreferrer"
+        onClick={handleClick}
+      >
+        <span class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+          Licences
+        </span>
+      </a>
+      <a
+        class="block lg:grid col-span-12"
+        href="https://admin.exchange.microsoft.com/#/messagetrace"
+        rel="noopener noreferrer"
+        onClick={handleClick}
+      >
+        <span class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded--lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+          Tracking
+        </span>
+      </a>
+      <a
+        class="block lg:grid col-span-12"
+        href="https://admin.exchange.microsoft.com/#/mailboxes"
+        rel="noopener noreferrer"
+        onClick={handleClick}
+      >
+        <span class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+          Exchange
+        </span>
+      </a>
+      <a
+        class="block lg:grid col-span-12"
+        href="https://admin.microsoft.com/Adminportal/Home?source=applauncher#/users"
+        rel="noopener noreferrer"
+        onClick={handleClick}
+      >
+        <span class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+          Users
+        </span>
+      </a>
     </div>
   );
 }
